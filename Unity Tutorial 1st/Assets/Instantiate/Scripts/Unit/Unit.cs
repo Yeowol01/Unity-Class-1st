38bc66daf1dd98d224a5cf1a1888e4a2e0b00729
Yeowol01 <139342008+Yeowol01@users.noreply.github.com>
2023-12-21 19:46:03 +0900

    (2023.12.21) 상태 패턴 적용

--- Unity Tutorial 1st/Assets/Instantiate/Scripts/Unit/Unit.cs
+++ Unity Tutorial 1st/Assets/Instantiate/Scripts/Unit/Unit.cs
@@ -2,45 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State
+{
+    Move, 
+    Attack,
+    Die
+}
+
 public abstract class Unit : MonoBehaviour
-{    
+{
     [SerializeField] GameObject target;
+
+    [SerializeField] State state;
+
+    [SerializeField] Animator animator;
     [SerializeField] Vector3 direction;
-    [SerializeField] float speed = 5.0f;
+    [SerializeField] Vector3 targetDirection;
+
+    [SerializeField] float speed = 5.0f;   
 
     private void Awake()
     {
         target = GameObject.Find("Player");
+        animator = GetComponent<Animator>();
     }
 
     public void Update()
     {
-        Move();
+        switch (state)
+        {
+            case State.Move : Move();
+                break;
+            case State.Attack : Attack();
+                break;
+            case State.Die : Die();
+                break;
+        }
     }
 
     public virtual void Move()
     {
+        animator.SetBool("Attack", false);
+
         // 1. Target의 벡터 - 자신의 위치 벡터
         direction = target.transform.position - transform.position;
+        targetDirection = target.transform.position;
 
         // 2. y축을 0으로 설정합니다.
         direction.y = 0;
+        targetDirection.y = 0;
 
         // 3. 벡터의 정규화
         direction.Normalize();
         
         // 4. 특정한 대상을 바라봅니다.
-        transform.LookAt(target.transform);
+        transform.LookAt(targetDirection);
 
         // 5. 방향 벡터를 구한 값을 가지고 이동을 합니다.
         transform.position += direction * speed * Time.deltaTime;
     }
 
+    public virtual void Attack()
+    {
+        animator.SetBool("Attack", true);
+    }
+
+    public virtual void Die()
+    {
+
+    }
+
     // OnTriggerEnter() : Trigger 충돌이 되었을 때 이벤트를 호출하는 함수입니다.
     private void OnTriggerEnter(Collider other)
     {
-        speed = 0;
-        Debug.Log("OnTriggerEnter");
+        state = State.Attack;
     }
 
     // OnTriggerStay() : Trigger가 충돌 중일 때 이벤트를 호출하는 함수입니다.
@@ -52,6 +87,6 @@ public abstract class Unit : MonoBehaviour
     // OnTriggerExit() : Trigger와 충돌이 끝났을 때 이벤트를 호출하는 함수입니다.
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("OnTriggerExit");
+        state = State.Move;
     }
 }
